580004e7d91a94e03faf1955193b86ab1c2a67cd
jchang12345 <justinchang4317@gmail.com>
2022-06-30 16:10:01 -0700

    MY FIRST COMMIT ON THIS NEW ECOSYSTEM

--- MyFirstApp/MyFirstAppApp.swift
+++ MyFirstApp/MyFirstAppApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MyFirstAppApp: App {
     var body: some Scene {
         WindowGroup {
+            //my first test comment and commit just to checkerino it outerino
             ContentView()
         }
     }
